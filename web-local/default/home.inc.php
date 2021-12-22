53c51f456374a4038227acb759efd2e79a6635ca
Peter Krauss <ppkrauss@gmail.com>
2021-12-22 08:40:50 -0300

    reestruturando como hotsite

---
+++ web-local/default/home.inc.php
@@ -0,0 +1,34 @@
+<!-- START MAIN CONTAINER -->
+<section class="main-figure">
+	<div class="main-text">
+		<span class="title">Direcciones de Colombia</span>
+
+<h2>&nbsp; Evento</h2>
+<p>&nbsp; <a href="https://local.addressforall.org/evento">Webinar miercoles 22 de diciembre de 2021</a>,<br/>&nbsp; 14:00 en hora de Colombia (GMT-5)</p>
+
+<h2>&nbsp;Mapa</h2>
+<p>&nbsp; <a target="_mapa" href="https://local.addressforall.org/map">Búsqueda de direcciones en el mapa</a></p>
+
+	</div>
+</section>
+<!-- END MAIN CONTAINER  -->
+
+<br/>
+<!-- START VIDEO -->
+<h2>Misión AddressForAll</h2>
+<section class="section section-dark">
+	<div class="dark-texto">
+		<p>
+Para que los <b>servicios de emergencia</b> lleguen al lugar correcto, para permitir realizar un análisis <b>cartográfico</b> en unos pocos clics,
+o incluso para que los operadores <b>públicos y privados</b> coordinen mejor sus trabajos,
+las direcciones son una verdadera cuestión de <b>soberanía</b> para Colombia y cualquier otro país.
+		</p>
+	</div>
+	<div class="responsive-container">
+		<iframe src="https://www.youtube.com/embed/WglOrCDKjeo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="responsive-video">
+		</iframe>
+	</div>
+</section>
+<!-- END VIDEO -->
+<!-- START CARDS -->
+
